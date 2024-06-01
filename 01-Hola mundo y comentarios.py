66483169d80e53a8d17da0b64d7eb8ee1b0bfc30
YoendiGY <yoendidev@gmail.com>
2024-05-31 23:33:58 -0400

    Add files via upload

---
+++ 01-Hola mundo y comentarios.py
@@ -0,0 +1,13 @@
+print('Hola mundo')
+
+#Esto es un comentario de una linea
+
+"""
+Esto es un comentario
+de varios
+saltos de 
+linea
+"""
+
+#Dentro de los parentesis usando comillas simples '' o dobles "" pones el mensaje que deseas mostrar por consola
+print('Adios') 
