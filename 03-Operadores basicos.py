b3aa5d204525374b14b14ac9426aca8b041382df
YoendiGY <yoendidev@gmail.com>
2024-06-02 13:21:54 -0400

    Add files via upload

---
+++ 03-Operadores basicos.py
@@ -0,0 +1,56 @@
+#Operadores aritmeticos
+
+#Suma
+suma = 5+4
+
+#Resta
+resta = 5-3
+
+#Multiplicacion
+multiplicacion = 4*5
+
+#Division
+division = 4/5
+
+#Modulo de la division
+modulo_division = 8%3
+
+#Potencia
+potencia = 4**3
+
+#Division entera
+division_entera = 8//3
+
+
+#Operadores logicos
+
+#Operador Y
+resY = True and False
+
+#Operador Or
+resOr = False or True
+
+#Operador de negacion
+resNegacion = not True
+
+
+#Operadores de comparacion
+
+#Igualdad
+resultadoIgualdad = 5==6
+
+#Desigualdad 
+resultadoDesigualdad = 4!=6
+
+#Mayor que
+mayor_que = 5>4
+
+#Menor que
+menor_que = 4<5
+
+#Mayor o igual que
+mayor_igual = 4>=7
+
+#Menor o igual 
+menor_igual = 4<=6
+
